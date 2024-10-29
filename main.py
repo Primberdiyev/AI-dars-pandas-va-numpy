b041710f559dca58472ed959bbaaccd92a31b54b
Primberdiyev <158053951+Primberdiyev@users.noreply.github.com>
2024-10-29 21:23:13 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print("Oddiy array:", arr)
+
+zero_array = np.zeros((2, 3))  
+ones_array = np.ones((3, 2))  
+print("0 lar array:", zero_array)
+print("1 lar array:", ones_array)
